a50165b02c7fbd9e309eed3f00d91fa0a358b651
gabengar <gabengar@protonmail.com>
2021-12-30 14:51:05 +0500

    feat: precommit hooks

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '**/*.[jt]s?(x)': (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(' --file ')}`,
+  "**/*": "prettier --write --ignore-unknown"
+}
