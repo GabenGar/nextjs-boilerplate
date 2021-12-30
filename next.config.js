c55ca94fe770841dbafe46404b624373669c6153
gabengar <gabengar@protonmail.com>
2021-12-30 12:18:43 +0500

    more sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextJSConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src", "styles")],
+  },
 }
+
+
+module.exports = nextJSConfig
