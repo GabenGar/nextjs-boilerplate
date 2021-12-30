a9613a6da4922d4bd91f73ae903b95189ccaa880
gabengar <gabengar@protonmail.com>
2021-12-30 14:16:54 +0500

    feat: ESlint

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextJSConfig = {
+  eslint: {
+    dirs: ["environment", "src"]
+  },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src", "styles")],
