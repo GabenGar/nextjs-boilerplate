4a889e04de269510384622706a5c3f121f5315d4
gabengar <gabengar@protonmail.com>
2022-01-03 20:08:24 +0500

    register endpoint

--- src/pages/api/v1/auth/register.ts
+++ src/pages/api/v1/auth/register.ts
@@ -4,11 +4,11 @@ import { ValidationErrors } from "#lib/json-schema/types";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { APIResponse } from "#types/api";
-import type { AccCreds } from "#types/entities";
+import type { AccCreds, Account } from "#types/entities";
 
 export default async function handleAccountRegistration(
   req: NextApiRequest,
-  res: NextApiResponse<APIResponse>
+  res: NextApiResponse<APIResponse<{ account: Account }>>
 ) {
   if (req.method === "POST") {
     try {
@@ -16,14 +16,18 @@ export default async function handleAccountRegistration(
       const result = await registerAccount(accCreds);
 
       if (result instanceof ValidationErrors) {
-        res.status(422).json({ success: false, errors: result.toDict() });
+        return res
+          .status(422)
+          .json({ success: false, errors: result.toDict() });
       }
 
       if (result instanceof AuthError) {
-        res.status(400).json({ success: false, errors: [result.message] });
+        return res
+          .status(400)
+          .json({ success: false, errors: [result.message] });
       }
 
-      res.status(200).json({ success: true });
+      return res.status(200).json({ success: true, data: { account: result } });
     } catch (error) {
       console.error(error);
       res.status(500).json({ success: false });
