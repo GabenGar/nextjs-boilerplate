c96932635b62eacb402814f4d0e9c86cc36e4785
gabengar <gabengar@protonmail.com>
2022-01-04 16:58:17 +0500

    feat: basic login logic

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
+import { AuthError } from "#types/errors";
+import { getReqBody } from "#lib/util";
+import { loginAccount, validateAccountFields } from "#lib/account";
 import { Form } from "#components/forms";
+import { ErrorsView } from "#components/errors";
 import {
   FormSectionPassword,
   FormSectionText,
 } from "#components/forms/sections";
 
+import type { BasePageProps } from "#types/pages";
+import type { AccCreds } from "#types/entities";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export function LoginPage() {
+interface LoginPageProps extends BasePageProps {
+  accCreds?: AccCreds;
+}
+
+export function LoginPage({
+  errors,
+  accCreds,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -15,7 +29,12 @@ export function LoginPage() {
       </Head>
       <h1>Login</h1>
       <Form method="POST">
-        <FormSectionText id="acc-name" name="name" required>
+        <FormSectionText
+          id="acc-name"
+          name="name"
+          required
+          defaultValue={accCreds?.name}
+        >
           Name
         </FormSectionText>
         <FormSectionPassword
@@ -23,12 +42,54 @@ export function LoginPage() {
           name="password"
           required
           isNew={false}
+          defaultValue={accCreds?.password}
         >
           Password
         </FormSectionPassword>
+        {errors && <ErrorsView errors={errors} />}
       </Form>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<LoginPageProps> = async ({
+  req,
+}) => {
+  if (req.method === "POST") {
+    const accCreds = await getReqBody<AccCreds>(req);
+    const { isValid, errors } = validateAccountFields(accCreds);
+
+    if (!isValid) {
+      return {
+        props: {
+          errors: errors!.toDict(),
+          accCreds,
+        },
+      };
+    }
+
+    const result = await loginAccount(accCreds);
+
+    if (result instanceof AuthError) {
+      return {
+        props: {
+          errors: [result.message],
+          accCreds,
+        },
+      };
+    }
+
+    return {
+      redirect: {
+        destination: "/account",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 export default LoginPage;
