5843fdb4d44a431cab2d57806635e06f0c4ea84d
GabenGar <87906913+GabenGar@users.noreply.github.com>
2022-01-08 09:27:24 +0500

    fix: prod database (#2)

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,20 +1,37 @@
 import pgLib from "pg-promise";
-import { DATABASE_URL } from "#environment/derived-vars";
+import {
+  DATABASE_HOSTNAME,
+  DATABASE_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_PORT,
+  DATABASE_USER,
+} from "#environment/env-vars";
+import { IS_DEVELOPMENT } from "#environment/derived-vars";
 import { createSingleton } from "#lib/util";
 
 import type { IInitOptions, IDatabase, IMain } from "pg-promise";
+import type {
+  IClient,
+  IConnectionParameters,
+} from "pg-promise/typescript/pg-subset";
 
 interface IDatabaseScope {
   db: IDatabase<any>;
   pgp: IMain;
 }
 
-const initOptions: IInitOptions = {
-
+const initOptions: IInitOptions = {};
+const connParams: IConnectionParameters<IClient> = {
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  database: DATABASE_NAME,
+  port: Number(DATABASE_PORT),
+  host: DATABASE_HOSTNAME,
+  ssl: !IS_DEVELOPMENT,
 };
 const pgp = pgLib(initOptions);
 // return date strings as strings
-pgp.pg.types.setTypeParser(1114, dateString=>dateString)
+pgp.pg.types.setTypeParser(1114, (dateString) => dateString);
 
 /**
  * @link https://stackoverflow.com/questions/34382796/where-should-i-initialize-pg-promise#answer-34427278
@@ -22,7 +39,7 @@ pgp.pg.types.setTypeParser(1114, dateString=>dateString)
 export function getDBSingleton(): IDatabaseScope {
   return createSingleton<IDatabaseScope>("db-scope", () => {
     return {
-      db: pgp(DATABASE_URL),
+      db: pgp(connParams),
       pgp,
     };
   });
